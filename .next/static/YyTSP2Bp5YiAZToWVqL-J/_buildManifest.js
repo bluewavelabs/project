276aca2a7899ce3c22819daf38f621b4cbeb6ba4
bluewave <admin@bluewavelabs.kr>
2025-10-28 12:06:23 +0900

    수정

---
+++ .next/static/YyTSP2Bp5YiAZToWVqL-J/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/253-9eb89de929d80930.js","static/chunks/pages/index-1b27d22c1bd614aa.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
